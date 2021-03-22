f8ab1f1f85d4388c62e7db6c7fd1adbdb974107f
Mailyn Angarita <79997083+mayangarita@users.noreply.github.com>
2021-03-21 22:57:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
+// Activador de jQuery - Función principal de jQuery
 $(document).ready(function(){
-                
-    $('#cerrar').hide();
-    
-    $('#abrir').click(function(){
-        $('#menu-cortina').animate({
+
+    // Activación de transición del menú principal y del menú móvil
+    $('header article nav a, aside#menu-moviles nav a').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000,'easeOutExpo');
+        event.preventDefault();
+    });
+
+    // Funciones de Abrir y Cerrar el Menú Responsive
+    $('a#abrir-menu').click(function(){
+        $('aside#menu-moviles').animate({
             right: 0
-        },300);
-        $('#abrir').hide();
-        $('#cerrar').show();
+        },500,'easeOutExpo');
+
+        $('a#abrir-menu').hide();
+        $('a#cerrar-menu').show();
+
         event.preventDefault();
-});
-                       
-$('#cerrar').click(function(){
-        $('#menu-cortina').animate({
-            right: -499
-        },300);
-        $('#cerrar').hide();
-        $('#abrir').show();
+    });
+
+    $('a#cerrar-menu, aside#menu-moviles nav a').click(function(){
+        $('aside#menu-moviles').animate({
+            right: -290
+        },1000,'easeOutElastic');
+        
+        $('a#abrir-menu').show();
+        $('a#cerrar-menu').hide();
+
         event.preventDefault();
-});
+    });
+    
 });
